683933d42f196e3b688bf014f507a75658fe65aa
Quamalt <136390960+Quamalt@users.noreply.github.com>
2023-06-19 19:13:12 -0400

    Update and rename server to server.js

---
+++ server.js
@@ -0,0 +1,7 @@
+function learn_u_p(){
+    var user = document.getElementById("user").value;
+    var pass = document.getElementById("pass").value;
+    // save user and pass to text file
+    // append user and pass to text file save text file
+    console.log(user, pass);
+}
